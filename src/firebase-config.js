c94590da4f4ae7ba6b7743d6bae92f02888a6203
Srilekha-03 <srilekhakasha47@gmail.com>
2024-10-09 00:07:53 +0530

    added env security

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from '@firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDqZqI281kdFF4BSSmpox3vIZ3nYDuOvHY",
-    authDomain: "fir-tut-1c559.firebaseapp.com",
-    projectId: "fir-tut-1c559",
-    storageBucket: "fir-tut-1c559.appspot.com",
-    messagingSenderId: "151195930",
-    appId: "1:151195930:web:d09e0f9d86c70ebf761ca3",
-    measurementId: "G-XDKMY7184M"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
 
 const app = initializeApp(firebaseConfig);
 
